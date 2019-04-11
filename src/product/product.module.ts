b00c493e573c2e3dd7c72bbad4171396fece5446
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-04-11 16:34:58 +0200

    Import schema inside the module

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,8 +1,13 @@
+import { ProductSchema } from './schemas/product.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 
 @Module({
+  imports: [ MongooseModule.forFeature([
+    {name: 'Product', schema: ProductSchema},
+  ])],
   controllers: [ProductController],
   providers: [ProductService]
 })
