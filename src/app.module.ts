c20012683bd09df93f572862967f7abd21d6d95b
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-04-11 16:26:50 +0200

    Define Database Connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ProductModule,
+    MongooseModule.forRoot(
+      'mongodb://user:password@localhost:27017/test',
+      {useNewUrlParser: true}
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
