a6113fc278e8245c11b9a8f461084857c143c05c
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-04-11 15:47:52 +0200

    Dummy Post

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Res, HttpStatus } from '@nestjs/common';
+import { resolve } from 'path';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+
+    @Post('/create')
+    createPost(@Res() res) {
+        res.status(HttpStatus.OK).json(
+            {
+                message: 'received',
+            }
+        );
+    }
+
+}
