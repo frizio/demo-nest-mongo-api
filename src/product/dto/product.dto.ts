15f00c27b3a221a48879d949c14eedf8485041d6
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-04-11 15:59:16 +0200

    Create product DTO structure

--- src/product/dto/product.dto.ts
+++ src/product/dto/product.dto.ts
@@ -1,1 +1,8 @@
-export class ProductDto {}
+export class ProductDto {
+    readonly name: string;
+    readonly description: string;
+    readonly imageUrl: string;
+    readonly price: number;
+    readonly new: boolean;
+    readonly createdAt: Date;
+}
