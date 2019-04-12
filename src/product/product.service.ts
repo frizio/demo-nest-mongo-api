a70a4856df9187e26a748fe8b8091d6ed869616a
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-04-12 14:26:00 +0200

    Create scheleton of ProductService

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Product } from './interfaces/product.interface';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+
+    constructor(
+        @InjectModel('Product')
+        private readonly productModel: Model<Product>,
+    ) {}
+
+    getProducts() {}
+
+    getProduct() {}
+
+    createProduct() {}
+
+    deleteProduct() {}
+
+    updateProduct() {}
+
+}
