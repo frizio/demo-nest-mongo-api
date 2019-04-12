7404448f8cdda44b9d1b8b6113d957f3be4f88e1
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-04-12 14:09:33 +0200

    Add mongo url remote connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,16 @@ import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
+// const MONGO_URL = 'mongodb://user:password@localhost:27017/test';
+const MONGO_URL = 'mongodb+srv://frizio:p455w0rdq@friziodemocluster-oq16p.gcp.mongodb.net/test?retryWrites=true';
+
 
 @Module({
   imports: [
     ProductModule,
     MongooseModule.forRoot(
-      'mongodb://user:password@localhost:27017/test',
-      {useNewUrlParser: true}
+      MONGO_URL,
+      {useNewUrlParser: true},
     ),
   ],
   controllers: [AppController],
