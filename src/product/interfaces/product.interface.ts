e0c1f42d26d5ed3824b6ef10a7ee6b19e893e402
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-04-12 14:22:55 +0200

    Create product interface

--- src/product/interfaces/product.interface.ts
+++ src/product/interfaces/product.interface.ts
@@ -1,1 +1,10 @@
-export interface Product {}
+import { Document } from "mongoose";
+
+export interface Product extends Document {
+    readonly name: string;
+    readonly description: string;
+    readonly imageURL: string;
+    readonly price: number;
+    readonly new: boolean;
+    readonly createdAt: Date;
+}
